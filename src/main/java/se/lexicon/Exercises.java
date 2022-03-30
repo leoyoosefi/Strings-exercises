765bf054e83b4e1762e739aef6f92a3ee96c7b96
Leo Yoosefi <101973222+leoyoosefi@users.noreply.github.com>
2022-03-30 13:06:50 +0200

    String Exercises

---
+++ src/main/java/se/lexicon/Exercises.java
@@ -0,0 +1,94 @@
+package se.lexicon;
+
+import java.util.*;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+
+        /*/1
+        String java = "JAVA";
+        System.out.println(java.length());
+
+         */
+
+
+        /*/1.2
+        String sentence = "long example sentence";
+        System.out.println(sentence);
+        System.out.println("Answer is: " + sentence.charAt(6));
+
+         */
+
+
+        /*/2
+        String sent2 = "Even longer example sentence";
+
+        int index1 = sent2.indexOf('o');
+        System.out.println(index1);
+
+         */
+
+
+        /*/3
+        String sent3 = "Ok this is not as long!";
+
+        System.out.println(sent3.substring(11));
+
+         */
+
+        /*/4
+
+        String sent3 = "CAPS EQUALS SCREAMING";
+        System.out.println(sent3.toLowerCase());
+        System.out.println(sent3.toUpperCase());
+
+         */
+
+        /*/5
+
+
+        String sent4 = "Java is the worst programming language!";
+        System.out.println(sent4.replace("worst", "best"));
+
+         */
+
+        /*/6
+
+        String str = "\tJ\ta\tv\ta\t";
+        System.out.println(str.trim());
+
+
+         */
+
+        /*/7
+
+        int n = 20;
+        String n2 = String.valueOf(n);
+        System.out.println(n2 + "20");
+
+         */
+
+        //8
+
+
+        //9
+
+        String [] names = new String[]{"Carl,Susie,Fredrick,Bob,Erik"};
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
